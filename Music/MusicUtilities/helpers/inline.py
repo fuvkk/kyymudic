4023102884f7d5a7ecd81680a5c4de839f55caab
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-01-11 18:03:59 +0530

    Update inline.py

--- Music/MusicUtilities/helpers/inline.py
+++ Music/MusicUtilities/helpers/inline.py
@@ -11,6 +11,11 @@ from Music.config import GROUP, CHANNEL
 def play_markup(videoid, user_id):
     buttons = [
         [
+            InlineKeyboardButton(
+                text=f"{total_time} ------------------ {current_time}",
+                callback_data=f"timer_checkup_markup {videoid}|{user_id}",
+            )
+        ],[
             InlineKeyboardButton(text="▷", callback_data=f"resumevc2"),
             InlineKeyboardButton(text="II", callback_data=f"pausevc2"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"skipvc2"),
