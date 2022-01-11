018aade31ad2e0eb56497766dec9761a1ae45837
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-01-11 16:57:46 +0530

    Update play.py

--- Music/Plugins/play.py
+++ Music/Plugins/play.py
@@ -460,29 +460,17 @@ Saya perlu menjadi admin dengan beberapa izin:
         query = message.text.split(None, 1)[1]
         mystic = await message.reply_text("**🔎 Pencarian**")
         try:
-            a = VideosSearch(query, limit=5)
+            a = VideosSearch(query, limit=1)
             result = (a.result()).get("result")
             title1 = result[0]["title"]
             duration1 = result[0]["duration"]
-            title2 = result[1]["title"]
-            duration2 = result[1]["duration"]
-            title3 = result[2]["title"]
-            duration3 = result[2]["duration"]
-            title4 = result[3]["title"]
-            duration4 = result[3]["duration"]
-            title5 = result[4]["title"]
-            duration5 = result[4]["duration"]
             ID1 = result[0]["id"]
-            ID2 = result[1]["id"]
-            ID3 = result[2]["id"]
-            ID4 = result[3]["id"]
-            ID5 = result[4]["id"]
         except Exception as e:
             return await mystic.edit_text(
                 f"Lagu Tidak Ditemukan.\n**Kemungkinan Alasan:** {e}"
             )
         thumb ="cache/IMG_20211105_143948_192.jpg"
-        buttons = search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, duration4, duration5, user_id, query)
+        buttons = search_markup(ID1, duration1, user_id, query)
         await mystic.edit( 
             f"**✨ Silahkan pilih lagu yang ingin anda putar**\n\n¹ <b>{title1[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID1})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n² <b>{title2[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID2})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n³ <b>{title3[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID3})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n⁴ <b>{title4[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID4})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n⁵ <b>{title5[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID5})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__",    
             reply_markup=InlineKeyboardMarkup(buttons),
@@ -766,50 +754,15 @@ async def popat(_,CallbackQuery):
     i=int(id)
     query = str(query)
     try:
-        a = VideosSearch(query, limit=10)
+        a = VideosSearch(query, limit=1)
         result = (a.result()).get("result")
         title1 = (result[0]["title"])
         duration1 = (result[0]["duration"])
-        title2 = (result[1]["title"])
-        duration2 = (result[1]["duration"])      
-        title3 = (result[2]["title"])
-        duration3 = (result[2]["duration"])
-        title4 = (result[3]["title"])
-        duration4 = (result[3]["duration"])
-        title5 = (result[4]["title"])
-        duration5 = (result[4]["duration"])
-        title6 = (result[5]["title"])
-        duration6 = (result[5]["duration"])
-        title7= (result[6]["title"])
-        duration7 = (result[6]["duration"])      
-        title8 = (result[7]["title"])
-        duration8 = (result[7]["duration"])
-        title9 = (result[8]["title"])
-        duration9 = (result[8]["duration"])
-        title10 = (result[9]["title"])
-        duration10 = (result[9]["duration"])
-        ID1 = (result[0]["id"])
-        ID2 = (result[1]["id"])
-        ID3 = (result[2]["id"])
-        ID4 = (result[3]["id"])
-        ID5 = (result[4]["id"])
-        ID6 = (result[5]["id"])
-        ID7 = (result[6]["id"])
-        ID8 = (result[7]["id"])
-        ID9 = (result[8]["id"])
-        ID10 = (result[9]["id"])                    
+        ID1 = (result[0]["id"])                    
     except Exception as e:
         return await mystic.edit_text(f"Lagu Tidak Ditemukan.\n**Kemungkinan Alasan:**{e}")
-    if i == 1:
-        buttons = search_markup2(ID6, ID7, ID8, ID9, ID10, duration6, duration7, duration8, duration9, duration10 ,user_id, query)
-        await CallbackQuery.edit_message_text(
-            f"**✨ Silahkan pilih lagu yang ingin anda putar**\n\n⁶ <b>{title6[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID6})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n⁷ <b>{title7[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID7})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n⁸ <b>{title8[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID8})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n⁹ <b>{title9[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID9})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n¹⁰ <b>{title10[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID10})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__",    
-            reply_markup=InlineKeyboardMarkup(buttons),
-            disable_web_page_preview=True
-        )  
-        return    
     if i == 2:
-        buttons = search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, duration4, duration5, user_id, query)
+        buttons = search_markup(ID1, duration1, user_id, query)
         await CallbackQuery.edit_message_text(
             f"**✨ Silahkan pilih lagu yang ingin anda putar**\n\n¹ <b>{title1[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID1})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n² <b>{title2[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID2})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n³ <b>{title3[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID3})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n⁴ <b>{title4[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID4})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__\n\n⁵ <b>{title5[:27]}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID5})__</u>\n  ┗ ⚡ __Powered by {BOT_NAME}__",    
             reply_markup=InlineKeyboardMarkup(buttons),
