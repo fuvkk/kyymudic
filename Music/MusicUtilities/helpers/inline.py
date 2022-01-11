6aefcf07fbd9d7a93d19cea90eba15336c4b7024
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-01-11 17:01:30 +0530

    Update inline.py

--- Music/MusicUtilities/helpers/inline.py
+++ Music/MusicUtilities/helpers/inline.py
@@ -79,86 +79,21 @@ def audio_markup(videoid, user_id):
 
 def search_markup(
     ID1,
-    ID2,
-    ID3,
-    ID4,
-    ID5,
     duration1,
-    duration2,
-    duration3,
-    duration4,
-    duration5,
     user_id,
     query,
 ):
     buttons = [
         [
             InlineKeyboardButton(
-                text="¹", callback_data=f"Music2 {ID1}|{duration1}|{user_id}"
-            ),
-            InlineKeyboardButton(
-                text="²", callback_data=f"Music2 {ID2}|{duration2}|{user_id}"
-            ),
-            InlineKeyboardButton(
-                text="³", callback_data=f"Music2 {ID3}|{duration3}|{user_id}"
+                text="Click Here", callback_data=f"Music2 {ID1}|{duration1}|{user_id}"
             ),
         ],
-        [
-            InlineKeyboardButton(
-                text="⁴", callback_data=f"Music2 {ID4}|{duration4}|{user_id}"
-            ),
-            InlineKeyboardButton(
-                text="⁵", callback_data=f"Music2 {ID5}|{duration5}|{user_id}"
-            ),
-        ],
-        [InlineKeyboardButton(text="⪼", callback_data=f"popat 1|{query}|{user_id}")],
         [
             InlineKeyboardButton(
                 text="ᴛᴜᴛᴜᴘ", callback_data=f"ppcl2 smex|{user_id}"
             ),
         ],
-    ]
-    return buttons
-
-
-def search_markup2(
-    ID6,
-    ID7,
-    ID8,
-    ID9,
-    ID10,
-    duration6,
-    duration7,
-    duration8,
-    duration9,
-    duration10,
-    user_id,
-    query,
-):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="⁶", callback_data=f"Music2 {ID6}|{duration6}|{user_id}"
-            ),
-            InlineKeyboardButton(
-                text="⁷", callback_data=f"Music2 {ID7}|{duration7}|{user_id}"
-            ),
-            InlineKeyboardButton(
-                text="⁸", callback_data=f"Music2 {ID8}|{duration8}|{user_id}"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="⁹", callback_data=f"Music2 {ID9}|{duration9}|{user_id}"
-            ),
-            InlineKeyboardButton(
-                text="¹⁰", callback_data=f"Music2 {ID10}|{duration10}|{user_id}"
-            ),
-        ],
-        [
-            InlineKeyboardButton(text="⪻", callback_data=f"popat 2|{query}|{user_id}"),
-        ],
-        [InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data=f"ppcl2 smex|{user_id}")],
     ]
     return buttons
 
